43fd4862900eadaa7400c4c7c1bf907292772005
MLuizaOliva <marialuizaolivasantos@gmail.com>
2024-11-16 21:44:09 -0300

    CEPEDI

---
+++ country_code.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+country_to_code = {
+    'Switzerland': 40,      # Europa
+    'Turkey': 41,           # Europa
+    'Sweden': 42,           # Europa
+    'Canada': 21,           # América do Norte
+    'Italy': 43,            # Europa
+    'Mexico': 22,           # América do Norte
+    'Uruguay': 3,           # América do Sul
+    'Spain': 44,            # Europa
+    'Czech Republic': 45,   # Europa
+    'Indonesia': 60,        # Ásia
+    'Australia': 80,        # Oceania
+    'Colombia': 4,          # América do Sul
+    'British Virgin Islands': 23,  # América do Norte
+    np.nan: 0,               # Não especificado
+    'Netherlands': 46,      # Europa
+    'United States': 20,    # América do Norte
+    'France': 47,           # Europa
+    'Anguilla': 24,         # América do Norte
+    'Puerto Rico': 25,      # América do Norte
+    'United Kingdom': 48,   # Europa
+    'Germany': 49,          # Europa
+    'Brazil': 1,            # América do Sul
+    'United Arab Emirates': 61,  # Ásia
+    "CÃ´te d'Ivoire": 100,   # África
+    'Russia': 62,           # Ásia
+    'India': 63             # Ásia
+}
